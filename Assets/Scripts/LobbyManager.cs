2f887d7de12eef08869145472e9a267b8785ab3d
Lucas Thielmann <thielmlw@mail.uc.edu>
2022-11-20 13:46:07 -0500

    finished up lobbymanager functionality

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -7,16 +7,15 @@ using Unity.Netcode;
 public class LobbyManager : NetworkBehaviour
 {
     private List<LobbyPlayerPanel> playerPanels;
+    private ulong[] playerIDs;
 
     public GameObject playerScrollContent;
-    public TMPro.TMP_Text txtPlayerNumber;
-    public Button btnStart;
-    public Button btnReady;
     public LobbyPlayerPanel playerPanelPrefab;
 
     public void Awake()
     {
         playerPanels = new List<LobbyPlayerPanel>();
+        playerIDs = new ulong[0];
     }
 
     private void AddPlayerPanel(ulong clientID)
@@ -44,11 +43,33 @@ public class LobbyManager : NetworkBehaviour
         }
     }
 
+    private void RefreshPlayerPanels(ulong[] ids)
+    {
+        Debug.Log("Refresh called");
+        foreach (LobbyPlayerPanel panel in playerPanels)
+        {
+            Destroy(panel.gameObject);
+        }
+        playerPanels.Clear();
+
+        foreach (ulong cid in ids)
+        {
+            AddPlayerPanel(cid);
+        }
+    }
+
+    public void BtnStartClicked()
+    {
+        if (IsHost)
+            NetworkManager.SceneManager.LoadScene("ChrisTest", UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+
     //Events
     public override void OnNetworkSpawn()
     {
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
-        RefreshPlayerPanels();
+        if (IsHost)
+            RefreshPlayerPanels();
     }
 
     public override void OnDestroy()
@@ -58,7 +79,24 @@ public class LobbyManager : NetworkBehaviour
 
     private void OnClientConnected(ulong clientId)
     {
-        RefreshPlayerPanels();
+        if (IsHost)
+        {
+            playerIDs = new ulong[NetworkManager.Singleton.ConnectedClientsIds.Count];
+            for (int i = 0; i < NetworkManager.Singleton.ConnectedClientsIds.Count; i++)
+            {
+                playerIDs[i] = NetworkManager.Singleton.ConnectedClientsIds[i];
+            }
+
+            RefreshPlayerPanelsClientRpc(playerIDs);
+        }
+    }
+
+    //RPCS
+    [ClientRpc]
+    public void RefreshPlayerPanelsClientRpc(ulong[] cids)
+    {
+        Debug.Log("Received refresh rpc");
+        RefreshPlayerPanels(cids);
     }
 
 }
