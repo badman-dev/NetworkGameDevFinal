84535525cb076856370ccc7cc0a6040e5a423961
Chris Brady <budmandev@gmail.com>
2022-12-09 16:00:13 -0500

    clients can dictate their own, non-damaging bullet

--- Assets/Scripts/DamageSource.cs
+++ Assets/Scripts/DamageSource.cs
@@ -6,14 +6,30 @@ using Unity.Netcode;
 public class DamageSource : MonoBehaviour
 {
     public int damage = 1;
+    public List<ulong> omitList = new List<ulong>();
     public bool destroyAfterCollision = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (destroyAfterCollision && (!collision.gameObject.GetComponent<NetworkObject>() || collision.gameObject.GetComponent<NetworkObject>().OwnerClientId != gameObject.GetComponent<NetworkObject>().OwnerClientId))
+        if (destroyAfterCollision)
         {
-            Physics2D.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider2D>());
-            Destroy(gameObject);
+            if (!collision.gameObject.GetComponent<NetworkObject>())
+            {
+                Physics2D.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider2D>());
+                Destroy(gameObject);
+            }
+            else
+            {
+                foreach(ulong playerId in omitList)
+                {
+                    if (collision.gameObject.GetComponent<NetworkObject>().OwnerClientId != playerId)
+                    {
+                        Physics2D.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider2D>());
+                        Destroy(gameObject);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
